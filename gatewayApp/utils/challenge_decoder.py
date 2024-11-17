a900c5dff728dceac416838c34172a2cd40cb7ac
Tymoteusz Kosman <tkosman03@gmail.com>
2024-11-17 23:02:25 +0100

    Refactor authentication logic to generate and encrypt challenges
    The authentication logic in the gateway service has been refactored to generate challenges for user authentication and encrypt them using the user's public key. This change improves the security of the authentication process by ensuring that client will not sign disinheritance of his child.

--- gatewayApp/utils/challenge_decoder.py
+++ gatewayApp/utils/challenge_decoder.py
@@ -14,6 +14,17 @@ def load_private_key(pem_data):
         password=None
     )
 
+def decrypt_challenge(private_key, challenge):
+    """Decrypt the challenge using the private key."""
+    return private_key.decrypt(
+        challenge,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
 def sign_challenge(private_key, challenge):
     """Sign the challenge using the private key."""
     return private_key.sign(
@@ -29,12 +40,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sign a challenge with a private key.')
     parser.add_argument('-k', '--private_key', required=True, help='Path to the PEM private key file.')
     parser.add_argument('-c', '--challenge', required=True, help='Challenge to be signed.')
+    parser.add_argument('-d', '--decrypt', action='store_true', help='Decrypt the challenge instead of signing it.')
     args = parser.parse_args()
 
     with open(args.private_key, 'r', encoding='utf-8') as f:
         private_key_pem = f.read()
 
     private_key = load_private_key(private_key_pem)
-    challenge_bytes = bytes.fromhex(args.challenge)
-    signed_challenge = sign_challenge(private_key, challenge_bytes)
-    print("Signed Challenge (hex):", signed_challenge.hex())
+    challenge_bytes = args.challenge.encode() if not args.decrypt else bytes.fromhex(args.challenge)
+
+    if args.decrypt:
+        decrypted_challenge = decrypt_challenge(private_key, challenge_bytes)
+        print("Decrypted Challenge:", decrypted_challenge.decode())
+    else:
+        signed_challenge = sign_challenge(private_key, challenge_bytes)
+        print("Signed Challenge (hex):", signed_challenge.hex())
