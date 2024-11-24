909aff8e23152eea884910680af37f5ec12d15cb
wyz3r0 <dobroszek.d@gmail.com>
2024-11-24 23:44:51 +0100

    Connected node_connection_client to the app structure

--- gatewayApp/gateway/__main__.py
+++ gatewayApp/gateway/__main__.py
@@ -2,12 +2,18 @@
 Sanic app.
 """
 import argparse
+import os
+from queue import Queue
+import sys
+from threading import Thread
+import time
 
 import sanic
 from sanic import response, request
 from sanic_ext import openapi
 from sanic_ext import validate
 from sanic_ext.exceptions import ValidationError
+
 from .auth import protected
 from .auth import authenticate
 from .auth import register
@@ -18,6 +24,11 @@ from .valid_schemas import ValidChallengeRequest
 from .valid_schemas import ValidAuthRequest
 
 from .daemonize import daemonize
+
+from .node_connection_client import NodeConnectionClient
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Message')))
+from Message import Message, Type
 
 __version__ = '1.0.0'
 
@@ -60,7 +71,7 @@ def attach_endpoints(app):
     )
     @validate(json=ValidUser)
     def handle_register(request, body: ValidUser):
-        return register(body)
+        return register(app.ctx.message_queue, body)
 
     @app.route('/challenge', methods=["POST"])
     @openapi.definition(
@@ -73,7 +84,7 @@ def attach_endpoints(app):
     )
     @validate(json=ValidChallengeRequest)
     def handle_challenge(request, body: ValidChallengeRequest):
-        return generate_challenge(request, body)
+        return generate_challenge(app.ctx.message_queue, request, body)
 
     @app.route('/auth', methods=["POST"])
     @openapi.definition(
@@ -87,7 +98,7 @@ def attach_endpoints(app):
     )
     @validate(json=ValidAuthRequest)
     def handle_authentication(request, body: ValidAuthRequest):
-        return authenticate(request, body)
+        return authenticate(app.ctx.message_queue, request, body)
 
     @app.exception(ValidationError)
     def handle_invalid_request(request, exception):
@@ -110,12 +121,34 @@ def attach_endpoints(app):
     def protected_endpoint(request):
         return response.json({"message": "This is a protected endpoint."})
 
+def background_connection_worker(queue: Queue):
+    try:
+        node_connection_client = NodeConnectionClient(queue)
+    except ConnectionError as e:
+        # TODO : handle error
+        print(e)
+
 def create_app(arguments):
     "Sanic app factory."
     app = sanic.Sanic("Gateway")
     app.ctx.args = arguments
     app.ctx.challenges = {}
     app.config.SECRET = "secret" #TODO: change this to a more secure secret
+
+    app.ctx.message_queue = Queue()
+
+    @app.before_server_start()
+    async def node_connection_manager(app):
+        app.ctx.bg_thread = Thread(target=background_connection_worker, args=(app.ctx.message_queue,))
+        app.ctx.bg_thread.daemon = True
+        app.ctx.bg_thread.start()
+
+    @app.before_server_stop
+    async def stop_background_thread(app, loop):
+        print("Stopping Node connection manager thread...")
+        app.ctx.message_queue.put(Message(type=Type.EXIT))
+        app.ctx.bg_thread.join()
+        print("Node connection manager thread stopped.")
 
     attach_endpoints(app)
     return app
--- gatewayApp/gateway/node_connection.py
+++ gatewayApp/gateway/node_connection.py
@@ -1,3 +1,10 @@
+import os
+from queue import Queue
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Message')))
+from Message import Message, Type
+
 """
 This module is a mock of the NODE service. It stores public keys of users.
 """
@@ -5,16 +12,32 @@ This module is a mock of the NODE service. It stores public keys of users.
 
 public_keys = {}
 
-def add_public_key(user_id, public_key):
+def send(queue: Queue, message: Message):
+    """Passes the message to node_communication_client
+    and sends it.
+
+    Args:
+        queue (Queue): Message queue.
+        message (Message): Message to send.
+    """
+    queue.put(message)
+
+def add_public_key(queue: Queue, user_id, public_key):
     """Adds a user's public key to the NODE service if it doesn't already exist."""
+    send(queue, Message(type=Type.REQUEST, payload=str(user_id)))
+
     if user_id in public_keys:
         raise ValueError("User ID already exists.")
     public_keys[user_id] = public_key
 
-def get_public_key(user_id):
+def get_public_key(queue: Queue, user_id):
     """Retrieve the public key of a given user_id."""
+    send(queue, Message(type=Type.REQUEST, payload=str(user_id)))
+
     return public_keys.get(user_id, None)
 
-def user_exists(user_id):
+def user_exists(queue: Queue, user_id):
     """Check if a user_id exists in NODE."""
+    send(queue, Message(type=Type.REQUEST, payload=str(user_id)))
+
     return user_id in public_keys
