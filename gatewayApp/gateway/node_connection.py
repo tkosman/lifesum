026f3a11b0f7df7c74f7ef01654ae7fe5ffb36ee
Tymoteusz Kosman <tkosman03@gmail.com>
2024-12-09 12:39:10 +0100

    First try of defining the main endpoints for the gateway

--- gatewayApp/gateway/node_connection.py
+++ gatewayApp/gateway/node_connection.py
@@ -33,9 +33,42 @@ def get_public_key(node_connection_client: NodeConnectionClient, user_id):
 
 def user_exists(node_connection_client: NodeConnectionClient, user_id):
     """Check if a user_id exists in NODE."""
-    node_connection_client.send(Message(type=Type.REGISTER, payload=str(user_id)))
-    response = node_connection_client.receive().to_json()
-    print(response)
+    return user_id in public_keys
 
-    return response
-    # return user_id in public_keys
+def want_to_become_expert_in_field(user_id, field):
+    """Send request to become expert in a given field."""
+
+    # Return True - if request succesffully sent to NODE
+    # Return False - if request failed
+    return True #TODO Dobrek - implement this
+
+def check_expert_in_field(user_id, field):
+    """Check if user is an expert in a given field."""
+
+    # Return True - if user is an expert in the given field
+    # Return False - if user is not an expert in the given field
+    return True #TODO Dobrek - implement this
+
+def open_expert_case(user_id, case_name):
+    """Open a case for an expert."""
+
+    # Return True - if case was opened successfully
+    # Return False - if case opening failed
+    return True #TODO Dobrek - implement this
+
+def get_open_expert_cases(user_id):
+    """Get all open cases for an expert."""
+
+    # Return list of open cases for given user_id
+    return [] #TODO Dobrek - implement this
+
+def add_item(category, itemInfo, owner_public_key):
+    """Add an item to the NODE service."""
+    # Add the item to the NODE service
+    # Return True - if item was added successfully
+    # Return False - if item addition failed
+    return True #TODO Dobrek - implement this
+
+def get_items():
+    """Get all items from the NODE service."""
+    return ["item1", "item2", "item3"] #TODO Dobrek - implement this
