3193aa4a51efaf98360ae586766c973109aeb175
budzu1 <147948018+budzu1@users.noreply.github.com>
2024-12-18 21:18:51 +0100

    quick fixes
    deployed all contracts
    provider added to blockchain_manager.py
    deploy for EC modified

--- nodeApp/scripts/deploy_expert_case_manager.py
+++ nodeApp/scripts/deploy_expert_case_manager.py
@@ -3,6 +3,16 @@ from ape import accounts, project, networks
 def main():
     account = accounts.load("accountNode")
 
+    reputation_manager_address = "0xFD96e13a1fE066B3C3F48013237B6b3CB084E345"
+    user_registry_address = "0xc406212e17e862c285386406c1307d34cd26d50D"
+
     with networks.ethereum.sepolia.use_provider("infura"):
-        contract = account.deploy(project.ExpertCaseManager)
-        print(f"Contract deployed at: {contract.address}")
+        contract = account.deploy(
+            project.ExpertCaseManager,
+            reputation_manager_address,
+            user_registry_address
+        )
+        print(f"ExpertCaseManager deployed at: {contract.address}")
+
+if __name__ == "__main__":
+    main()
--- nodeApp/scripts/get_blockchain_manager.py
+++ nodeApp/scripts/get_blockchain_manager.py
@@ -3,7 +3,7 @@ from blockchain_manager import BlockchainManager
 def get_blockchain_manager():
     # Define the contract addresses
     USER_REGISTRY_ADDRESS = "0xc406212e17e862c285386406c1307d34cd26d50D"
-    EXPERT_CASE_MANAGER_ADDRESS = "0xYourExpertCaseManagerContractAddress"
+    EXPERT_CASE_MANAGER_ADDRESS = "0xd714f29aE0ce9063C2F3998c40537Dec1c1fbF07"
     ITEM_REGISTRY_ADDRESS = "0x8F5bC4A52A9B8569ca4980AB000ea3395CBf03C1"
     REPUTATION_MANAGER_ADDRESS = "0xFD96e13a1fE066B3C3F48013237B6b3CB084E345"
 
