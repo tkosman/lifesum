8f9d0e66fe6432b3b5858ffe156da1f72445319a
budzu1 <147948018+budzu1@users.noreply.github.com>
2025-01-11 23:36:38 +0100

    another quick test
    signing

--- nodeApp/scripts/blockchain_manager.py
+++ nodeApp/scripts/blockchain_manager.py
@@ -71,7 +71,7 @@ class BlockchainManager:
         """
         try:
             with networks.ethereum.sepolia.use_provider("infura"):
-                # sender = accounts.load("my_account")
+                sender = self.account
                 tx = self.user_registry.registerUser(nick, public_key, additional_data, is_bot, sender=sender)
                 tx.wait_for_confirmation()
                 return tx.return_value
