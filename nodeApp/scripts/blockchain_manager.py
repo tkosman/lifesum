5cfaafabb65b92681a50488079d9cde308983539
budzu1 <147948018+budzu1@users.noreply.github.com>
2025-01-12 15:38:06 +0100

    new functions for EC

--- nodeApp/scripts/blockchain_manager.py
+++ nodeApp/scripts/blockchain_manager.py
@@ -290,6 +290,91 @@ class BlockchainManager:
                 return tx.return_value
         except Exception as e:
             raise Exception(f"Open Expert Case failed: {e}")
+        
+    def EC_become_expert(self, field_id, min_reputation, bot_allowed, nick, test_answers):
+        try:
+            with networks.ethereum.sepolia.use_provider("infura"):
+                sender = self.account
+                tx = self.expert_case_manager.openExpertCase(
+                    0,
+                    field_id,
+                    min_reputation,
+                    self.get_user_public_key(nick),
+                    bot_allowed,
+                    test_answers,
+                    nick,
+                    "0",
+                    field_id,
+                    3,
+                    sender=sender
+                )
+                return tx.return_value
+        except Exception as e:
+            raise Exception(f"Open Expert Case failed: {e}")
+        
+    def EC_update_item_owner(self, item_id, field_id, min_reputation, bot_allowed, nick, EC_info, new_owner):
+        try:
+            with networks.ethereum.sepolia.use_provider("infura"):
+                sender = self.account
+                tx = self.expert_case_manager.openExpertCase(
+                    item_id,
+                    field_id,
+                    min_reputation,
+                    self.get_user_public_key(nick),
+                    bot_allowed,
+                    EC_info,
+                    nick,
+                    new_owner,
+                    field_id,
+                    0,
+                    sender=sender
+                )
+                return tx.return_value
+        except Exception as e:
+            raise Exception(f"Open Expert Case failed: {e}")
+        
+    def EC_update_item_info(self, item_id, field_id, min_reputation, bot_allowed, nick, EC_info, new_item_info):
+        try:
+            with networks.ethereum.sepolia.use_provider("infura"):
+                sender = self.account
+                tx = self.expert_case_manager.openExpertCase(
+                    item_id,
+                    field_id,
+                    min_reputation,
+                    self.get_user_public_key(nick),
+                    bot_allowed,
+                    EC_info,
+                    nick,
+                    new_item_info,
+                    field_id,
+                    1,
+                    sender=sender
+                )
+                return tx.return_value
+        except Exception as e:
+            raise Exception(f"Open Expert Case failed: {e}")
+        
+    def EC_modify_user_additional_info(self, field_id, min_reputation, bot_allowed, nick, EC_info, new_additional_info):
+        try:
+            with networks.ethereum.sepolia.use_provider("infura"):
+                sender = self.account
+                tx = self.expert_case_manager.openExpertCase(
+                    0,
+                    field_id,
+                    min_reputation,
+                    self.get_user_public_key(nick),
+                    bot_allowed,
+                    EC_info,
+                    nick,
+                    new_additional_info,
+                    field_id,
+                    2,
+                    sender=sender
+                )
+                return tx.return_value
+        except Exception as e:
+            raise Exception(f"Open Expert Case failed: {e}")
+    
 
     def cast_vote(self, ec_id, option, public_key):
         """
