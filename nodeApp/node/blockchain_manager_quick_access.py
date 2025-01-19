abd68608791b4e779d7add07af89da5b78c61bea
Wiktor <wiktor.bachta@gmail.com>
2025-01-19 23:31:57 +0100

    fix

--- nodeApp/node/blockchain_manager_quick_access.py
+++ nodeApp/node/blockchain_manager_quick_access.py
@@ -5,8 +5,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 from blockchain_manager import BlockchainManager
 from get_blockchain_manager import get_blockchain_manager
 
+
+
 bm = get_blockchain_manager()
-# a = bm.add_item("finalsuper", "finalxd", bm.get_user_public_key("a"))
-print(bm.get_item(19))
-print(bm.get_item(16))
-print(bm.get_item(4))
+print(bm.get_item(20))
+ec = bm.open_expert_case(20, 1, 0, 'some_key', True, "zmiana opisu", "", "nowiutki opis", 0, 1)
+#print(ec)
+#bm.add_expert_field(bm.get_nick_by_address('some_key'), 1)
+bm.cast_vote(ec, 0, 'some_key')
+bm.close_expert_case(ec)
+print(bm.get_item(20))
